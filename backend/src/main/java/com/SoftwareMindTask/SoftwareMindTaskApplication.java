ccfaf4e6e6d865d61a5d703c0a5bc7a9eb19452e
Adam <adamly6@gmail.com>
2023-06-23 23:38:46 +0200

    creating fresh java project

---
+++ backend/src/main/java/com/SoftwareMindTask/SoftwareMindTaskApplication.java
@@ -0,0 +1,13 @@
+package com.SoftwareMindTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareMindTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareMindTaskApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/SoftwareMindTask/SoftwareMindTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SoftwareMindTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareMindTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
